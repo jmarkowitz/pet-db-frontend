27c0a7232540f5ac72d86e8fb608a7c0822be871
jmarkowitz <47903419+jmarkowitz@users.noreply.github.com>
2023-04-05 23:03:28 -0400

    Update petdb.sql

--- petdb.sql
+++ petdb.sql
@@ -1,90 +1,5 @@
-CREATE DATABASE petdb;
+CREATE DATABASE IF NOT EXISTS petdb;
 USE petdb;
-
-
-CREATE TABLE Pet (
-   color varchar(50) NOT NULL,
-   vaccination varchar(50) NOT NULL,
-   sterilization varchar(50) NOT NULL,
-   borrowing_availability boolean NOT NULL,
-   age int,
-   breed_id int,
-   species_id int,
-   personality varchar(100),
-   pet_id int PRIMARY KEY
-);
-
-CREATE TABLE Pet_Owner (
-   first_name varchar(50) NOT NULL,
-   last_name varchar(50) NOT NULL,
-   age int,
-   city varchar(50),
-   state varchar(50),
-   zip int,
-   occupation varchar(50),
-   description text,
-   user_id int PRIMARY KEY
-);
-
-CREATE TABLE Borrower (
-   first_name varchar(50) NOT NULL,
-   last_name varchar(50) NOT NULL,
-   availability varchar(50),
-   occupation varchar(50),
-   age int,
-   description text,
-   city varchar(50),
-   state varchar(50),
-   zip int,
-   borrower_id int PRIMARY KEY
-);
-
-CREATE TABLE Borrower_Review (
-   review_text varchar(100),
-   pet_id int PRIMARY KEY
-);
-
-CREATE TABLE Event (
-   description text,
-   event_date date,
-   city varchar(50),
-   state varchar(50),
-   zip varchar(50),
-   event_id int PRIMARY KEY
-);
-
-CREATE TABLE Borrower_Pet_Preferences (
-    species_id int,
-    type_id int,
-    habit_id int,
-    borrower_id int PRIMARY KEY
-);
-
-CREATE TABLE Habits (
-    borrower_id int,
-    habit_id int,
-    description text
-);
-
-CREATE TABLE Owner_Events (
-   user_id int,
-   event_id int
-);
-
-CREATE TABLE Owner_Friends (
-    user_id int PRIMARY KEY,
-    friend_user_id int PRIMARY KEY
-);
-
-CREATE TABLE Friends (
-    friend_user_id int,
-    start_date date
-);
-
-CREATE TABLE Borrower_Borrow (
-    borrower_id int PRIMARY KEY,
-    pet_id int PRIMARY KEY
-);
 
 CREATE TABLE IF NOT EXISTS PetServiceProviders
 (
@@ -101,7 +16,7 @@ CREATE TABLE IF NOT EXISTS Experience
     career        varchar(100),
     qualification varchar(100),
     city          varchar(100),
-    state         varchar(2),
+    state         varchar(50),
     zip           INTEGER NOT NULL,
     CONSTRAINT fk_pspID
         FOREIGN KEY (psp_id) REFERENCES PetServiceProviders (user_id)
@@ -114,7 +29,13 @@ CREATE TABLE IF NOT EXISTS ProviderReview
     cust_id     INTEGER NOT NULL,
     rating      varchar(100),
     CHECK ( rating BETWEEN 1 AND 5),
-    description varchar(1000)
+    description varchar(1000),
+    CONSTRAINT fk_prvd_review
+        FOREIGN KEY (psp_id) REFERENCES PetServiceProviders (user_id)
+            ON UPDATE CASCADE ON DELETE restrict,
+    CONSTRAINT fk_user_review
+        FOREIGN KEY (cust_id) REFERENCES PetOwner (user_id)
+            ON UPDATE CASCADE ON DELETE restrict
 );
 
 CREATE TABLE IF NOT EXISTS Service
@@ -123,24 +44,164 @@ CREATE TABLE IF NOT EXISTS Service
     service_id     INTEGER PRIMARY KEY,
     type           varchar(100) NOT NULL,
     duration_hours INTEGER      NOT NULL,
-    service_date   datetime DEFAULT CURRENT_TIMESTAMP
+    service_date   datetime DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_userID
+        FOREIGN KEY (user_id) REFERENCES PetServiceProviders (user_id)
+            ON UPDATE CASCADE ON DELETE restrict
 );
 
 CREATE TABLE IF NOT EXISTS PreferredPetSpecies
 (
     service_id    INTEGER NOT NULL,
     species_id    INTEGER NOT NULL,
-    vaccination   BOOLEAN,
+    vaccination   BOOLEAN NOT NULL,
     age           INTEGER NOT NULL,
-    sterilization BOOLEAN
+    sterilization BOOLEAN,
+    CONSTRAINT fk_serviceID
+        FOREIGN KEY (service_id) REFERENCES Service (service_id)
+            ON UPDATE CASCADE ON DELETE restrict,
+    CONSTRAINT fk_prefspeciesID
+        FOREIGN KEY (species_id) REFERENCES PetSpecies (species_id)
+            ON UPDATE CASCADE ON DELETE restrict
+);
+
+CREATE TABLE IF NOT EXISTS Pet
+(
+    color                  varchar(50) NOT NULL,
+    vaccination            varchar(50) NOT NULL,
+    sterilization          varchar(50) NOT NULL,
+    borrowing_availability boolean     NOT NULL,
+    age                    int         NOT NULL,
+    species_id             int         NOT NULL,
+    personality            varchar(1000),
+    pet_id                 int PRIMARY KEY,
+    CONSTRAINT fk_pet_speciesID
+        FOREIGN KEY (species_id) REFERENCES PetSpecies (species_id)
+            ON UPDATE CASCADE ON DELETE restrict
+);
+
+CREATE TABLE IF NOT EXISTS PetSpecies
+(
+    species_id   INTEGER PRIMARY KEY,
+    species_name varchar(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS PetBreed
+(
+    species_id INTEGER     NOT NULL,
+    breed_id   INTEGER     NOT NULL,
+    breed_name varchar(50) NOT NULL,
+    CONSTRAINT fk_speciesID
+        FOREIGN KEY (species_id) REFERENCES PetSpecies (species_id)
+            ON UPDATE CASCADE ON DELETE restrict
+);
+
+CREATE TABLE IF NOT EXISTS PetOwner
+(
+    first_name  varchar(50) NOT NULL,
+    last_name   varchar(50) NOT NULL,
+    age         int,
+    city        varchar(50),
+    state       varchar(50),
+    zip         int,
+    occupation  varchar(50),
+    description text,
+    user_id     int PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS OwnerFriends
+(
+    user_id        int            NOT NULL,
+    friend_num     INTEGER UNIQUE NOT NULL,
+    friend_user_id int            NOT NULL,
+    start_date     datetime DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_friend_userID
+        FOREIGN KEY (user_id) REFERENCES PetOwner (user_id)
+            ON UPDATE CASCADE ON DELETE restrict
+
 );
 
 CREATE TABLE IF NOT EXISTS OwnerPets
 (
     user_id INTEGER NOT NULL,
     pet_num INTEGER NOT NULL,
-    pet_id  INTEGER NOT NULL
-)
+    pet_id  INTEGER NOT NULL,
+    CONSTRAINT fk_owner_userID
+        FOREIGN KEY (user_id) REFERENCES PetOwner (user_id)
+            ON UPDATE CASCADE ON DELETE restrict,
+    CONSTRAINT fk_petID
+        FOREIGN KEY (pet_id) REFERENCES Pet (pet_id)
+            ON UPDATE CASCADE ON DELETE restrict
+);
+
+CREATE TABLE IF NOT EXISTS Event
+(
+    description text,
+    event_date  date,
+    city        varchar(50),
+    state       varchar(50),
+    zip         varchar(50),
+    event_id    int PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS OwnerEvents
+(
+    user_id  int,
+    event_id int,
+    CONSTRAINT fk_eventID
+        FOREIGN KEY (event_id) REFERENCES Event (event_id)
+            ON UPDATE CASCADE ON DELETE restrict
+);
+
+CREATE TABLE IF NOT EXISTS Borrower
+(
+    first_name   varchar(50) NOT NULL,
+    last_name    varchar(50) NOT NULL,
+    availability varchar(50),
+    occupation   varchar(50),
+    age          int,
+    description  text,
+    city         varchar(50),
+    state        varchar(50),
+    zip          int,
+    borrower_id  int PRIMARY KEY,
+    review_id   INTEGER,
+    CONSTRAINT fk_review
+        FOREIGN KEY (review_id) REFERENCES BorrowerReview (review_id)
+);
+
+CREATE TABLE IF NOT EXISTS BorrowerReview
+(
+    review_text varchar(100),
+    pet_id      int,
+    review_id INTEGER PRIMARY KEY
+
+);
+
+CREATE TABLE IF NOT EXISTS BorrowerPetPreferences
+(
+    species_id  int,
+    type_id     int,
+    habit_id    int,
+    borrower_id int PRIMARY KEY,
+    CONSTRAINT fk_habitID
+        FOREIGN KEY (habit_id) REFERENCES Habits (habit_id)
+            ON UPDATE CASCADE ON DELETE restrict
+);
+
+CREATE TABLE IF NOT EXISTS Habits
+(
+    habit_id    int PRIMARY KEY ,
+    habit_num   int NOT NULL,
+    description varchar(1000)
+);
+
+CREATE TABLE IF NOT EXISTS BorrowerBorrow
+(
+    borrower_id int PRIMARY KEY,
+    pet_id      int PRIMARY KEY
+);
+
 
 
 
